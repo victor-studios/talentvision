1ec750cdd2db29197558dc1f887b3e3571532d17
Salman Ali <salmanali@Salmans-MacBook-Pro.local>
2023-06-24 18:37:18 +0500

    edits with responsiveness

--- README.md
+++
@@ -1,2 +0,0 @@
-# talentvision
-# talentvision
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		// Responsive
+		responsive: true,
+		items: 3,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [980, 3],
+		itemsTablet: [768, 2],
+		itemsMobile: [479, 1],
+	})
+})
